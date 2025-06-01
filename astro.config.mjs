27644ec2918ab439f41db86c0590fa5be2d1756c
Ansh <anshgupta721@gmail.com>
2025-06-01 17:24:26 -0500

    yerp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
-import { defineConfig, squooshImageService } from 'astro/config';
-import github from '@astrojs/github'
+import { defineConfig} from 'astro/config';
 
 export default defineConfig({
   integrations: [tailwind(), mdx(), icon({
